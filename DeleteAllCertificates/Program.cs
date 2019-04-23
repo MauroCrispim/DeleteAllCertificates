24ae45e8c940fd429181cbc35cf070dda7066758
Mauro Crispim <mauro.crispim@cm-vfxira.pt>
2019-04-23 15:09:41 +0100

    Add project files.

---
+++ DeleteAllCertificates/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace DeleteAllCertificates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Microsoft\\SystemCertificates\\My\\Certificates";
+
+            DirectoryInfo dd = new DirectoryInfo(path);
+            foreach (System.IO.FileInfo file in dd.GetFiles())
+            {
+
+                Console.WriteLine(file.Name);
+                file.Delete();
+            }
+
+            Console.Read();
+        }
+    }
+}
